4fddeb4170c0ea03dce39340ac7205317f63b043
Luca Malatesta <l.malatesta87@gmail.com>
2018-04-29 12:46:37 +0200

    Improved quality of messages in checkConsistency()

--- Taxonomy/03-checkConsistency.R
+++ Taxonomy/03-checkConsistency.R
@@ -1,33 +1,47 @@
 checkConsistency <- function(x){
   x<-as.data.frame(apply(x,2,as.factor))
+  message('Checking consistency: taxa with more than one parent will be printed...')
+  errors=0
   for(t in levels(x$Species)){
     if(length(levels(droplevels(x[which(x$Species==t),'Genus'])))>1){
-      print(cat(t,length(levels(droplevels(x[which(x$Species==t),'Genus'])))))
+      errors=errors+1
+      message(paste('Species',t,'has',length(levels(droplevels(x[which(x$Species==t),'Genus']))),'parents'))
     }
   }
   for(t in levels(x$Genus)){
     if(length(levels(droplevels(x[which(x$Genus==t),'Family'])))>1){
-      print(cat(t,length(levels(droplevels(x[which(x$Genus==t),'Family'])))))
+      errors=errors+1
+      message(paste('Genus',t,'has',length(levels(droplevels(x[which(x$Genus==t),'Family']))),'parents'))
     }
   }
   for(t in levels(x$Family)){
     if(length(levels(droplevels(x[which(x$Family==t),'Order'])))>1){
-      print(cat(t,length(levels(droplevels(x[which(x$Family==t),'Order'])))))
+      errors=errors+1
+      message(paste('Family',t,'has',length(levels(droplevels(x[which(x$Family==t),'Order']))),'parents'))
     }
   }
   for(t in levels(x$Order)){
     if(length(levels(droplevels(x[which(x$Order==t),'Class'])))>1){
-      print(cat(t,length(levels(droplevels(x[which(x$Order==t),'Class'])))))
+      errors=errors+1
+      message(paste('Order',t,'has',length(levels(droplevels(x[which(x$Order==t),'Class']))),'parents'))
     }
   }
   for(t in levels(x$Class)){
     if(length(levels(droplevels(x[which(x$Class==t),'Phylum'])))>1){
-      print(cat(t,length(levels(droplevels(x[which(x$Class==t),'Phylum'])))))
+      errors=errors+1
+      message(paste('Class',t,'has',length(levels(droplevels(x[which(x$Class==t),'Phylum']))),'parents'))
     }
   }
   for(t in levels(x$Phylum)){
     if(length(levels(droplevels(x[which(x$Phylum==t),'Kingdom'])))>1){
-      print(cat(t,length(levels(droplevels(x[which(x$Phylum==t),'Kingdom'])))))
+      errors=errors+1
+      message(paste('Phylum',t,'has',length(levels(droplevels(x[which(x$Phylum==t),'Kingdom']))),'parents'))
     }
+  }
+  if(errors==0){
+    message('All good. Taxon tree is fully consistent')
+  }else{
+    message(paste('Number of incosistencies found:',errors))
+    message('Please check the taxa listed above and try again')
   }
 }
